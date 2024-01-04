6361fc194b9b0909b5fcdc8909b50771e4b39c3d
Ashutosh Mishra <ashutosh.mishra@tothenew.com>
2024-01-04 15:51:47 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by TTNPL-3940 on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
